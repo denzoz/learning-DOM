9464036298ff4edbd54d2d261d294b1c065f6baf
denzoz <none>
2020-07-14 00:02:00 +0100

    used querySelector & also added more actions with buttons

--- page/index.js
+++ page/index.js
@@ -1,9 +1,32 @@
 alert("After pressing OK, rest of script will be executed");
+var target = document.firstElementChild.lastElementChild.querySelector("UL").lastElementChild;
 
 function changeText() {
-    var target = document.firstElementChild.lastElementChild.querySelector("UL").lastElementChild;
     target.innerHTML = "hi";
     target.style.color = "green";
 }
 
-document.getElementById("scriptTrigger").onclick = changeText();
+function changeTextBack() {
+    target.innerHTML = "Third";
+    target.style.color = "black";
+}
+
+function editLink () {
+    linkTag.innerHTML = "Google :)";
+}
+
+function undoChange() {
+    linkTag.innerHTML = "Google";
+}
+
+//document.getElementById("scriptTriggerOne").addEventListener("click", changeText);
+//document.getElementById("scriptTriggerTwo").onclick = changeTextBack;
+// alternatively: document.getElementById("scriptTriggerTwo").addEventListener("click", changeTextBack);
+
+document.querySelector("#scriptTriggerOne").onclick = changeText;
+document.querySelector("#scriptTriggerTwo").onclick = changeTextBack;
+
+// how to select anchor tag within element with id called list
+var linkTag = document.querySelector("#list a");
+
+document.getElementsByTagName("button")[2].onclick = editLink;
